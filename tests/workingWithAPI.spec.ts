c8e4b8bf22f5e78695fd5ff70cffb29f7e268b6f
BranislavManojlovic <branislav.manojlovic@vegait.rs>
2026-07-13 10:56:27 +0200

    PL-001: Added test example for Modifing API response.

--- tests/workingWithAPI.spec.ts
+++ tests/workingWithAPI.spec.ts
@@ -9,9 +9,27 @@ test.beforeEach(async ({ page }) => {
     });
   });
 
+  // Modifing API response for the GET request to /api/articles?limit=10&offset=0
+  await page.route('https://conduit-api.bondaracademy.com/api/articles?limit=10&offset=0', async route => {
+      const response = await route.fetch()
+      const responseBody = await response.json();
+      // Taking properties that we want to modify
+      responseBody.articles[0].title = 'Modified Title';
+      responseBody.articles[0].description = 'Modified Description';
+      // Finishing the request with modified response
+      await route.fulfill({
+        body: JSON.stringify(responseBody),
+      });
+
+  });
+
+
   await page.goto('https://conduit.bondaracademy.com/');
 });
 
 test('has title', async ({ page }) => {
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
+  // Check that the mocked tags are displayed on the page
+  await expect(page.locator('app-article-list h1').first()).toContainText('Modified Title');
+  await expect(page.locator('app-article-list p').first()).toContainText('Modified Description');
 });
